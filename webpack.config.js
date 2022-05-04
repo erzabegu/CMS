69f4d73ab3c6df6c15b3dae5aaec35e5dfeb3701
ErezaBegu <ereza.begu@axians.com>
2022-05-04 08:15:40 +0200

    test

--- webpack.config.js
+++ webpack.config.js
@@ -64,7 +64,7 @@ module.exports = {
             template: path.join(__dirname, 'public', 'index.html')
         }),
         new webpack.ProvidePlugin({
-            process: 'process/browser',
+            process: 'process/browser'
         }),
     ]
 }
